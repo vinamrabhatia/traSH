14b541384d8725b65b375c4327f10d9f391bb9bf
Vinamra Bhatia <vinamrabhatia8@gmail.com>
2018-01-28 02:52:52 +0530

    Added prototypes for internal commands; implemented changedir and input buffer

--- src/trash.c
+++ src/trash.c
@@ -1,4 +1,6 @@
 #include<stdio.h>
+#include<unistd.h>
+#include<stdlib.h>
 
 bool active=0;
 bool logging=0;
@@ -10,3 +12,70 @@ int unlog(char **arg);
 int viewcmdlog(char **arg);
 int viewoutlog(char **arg);
 int changedir(char **arg);
+
+int changedir(char **arg)
+{
+  if (arg[1] == NULL)
+    fprintf(stderr, "\"changedir\" expects arguments\n");
+  else if (chdir(arg[1]) != 0)//chdir defined in unistd, changes current working dir to args[1]
+    perror("chdir failed");//defined in stdio, prints str : error
+  return 1;
+}
+
+int ext(char **arg)
+{
+  active=0;
+}
+
+
+
+char *in(void)//reads input to buffer
+{
+  int bsz = 512;
+  int pos = 0;
+  char *buffer = malloc(sizeof(char) * bsz);
+  int ch;
+
+  if (!buffer)
+  {
+    fprintf(stderr, "Memory allocation for input buffer failed\n");
+    exit(-1);
+  }
+
+  while (1)
+  {
+    ch = getchar();
+    if (ch == EOF || ch == '\n')
+    {
+      buffer[pos] = '\0';
+      return buffer;
+    }
+    else
+    {
+      buffer[pos] = ch;
+    }
+    pos++;
+    if (pos >= bsz)
+    {
+      bsz += 512;
+      buffer = realloc(buffer, bsz);
+      if (!buffer)
+      {
+        fprintf(stderr, "extending memory allocation for input buffer failed\n");
+        exit(-1);
+      }
+    }
+  }
+}
+
+
+int main(int argc, char **argv)
+{
+  char *input;
+  char **arg;
+  while(1)
+  {
+    printf("traSH $ ");
+    input=in();
+  }
+}
