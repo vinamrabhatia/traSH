24bc176f1e328adf22e22f1430ddfc34680c167d
Vinamra Bhatia <vinamrabhatia8@gmail.com>
2018-02-22 19:14:14 +0530

    Added pipe, cleanup

--- src/trash.c
+++ src/trash.c
@@ -18,6 +18,7 @@ int viewcmdlog(char **arg);
 int viewoutlog(char **arg);
 int changedir(char **arg);
 int external(char **arg);
+int do_command(char **args, int pipes);
 
 /*int sil = open("stdinlog.txt", O_APPEND | O_WRONLY);
 int sol = open("stdoutlog.txt", O_APPEND | O_WRONLY);
@@ -31,43 +32,13 @@ int changedir(char **arg)
 		perror("chdir failed");//defined in stdio, prints str : error
 	return 1;
 }
-/*//trashed function to search path for execurtable. Instead execvp returning -1 will cause the program to try ./executable
-int isfile(char *path)//checks if argument is a file
+
+int log(char **arg)
 {
-	struct stat buf;
-	stat(path, &buf);
-	return S_ISREG(buf.st_mode)
+	logging=1;
+	//dup2(sil,fileno(stdin));
 }
-
-int exec_in_path(char *name)
-{
-	char *path = getenv("PATH");
-	char *item = NULL;
-	int found	= 0;
-	if (!path) 
-		return 0;
-	path = strdup(path);
-	char fullpath[4096];
-	for (item = strtok(path, ":"); (!found) && item; item = strtok(NULL, ":"))
-		{
-			sprintf(real_path, "%s/%s", item, name);
-			// printf("Testing %s\n", real_path);
-			if ( is_file(real_path) && !(access(real_path, F_OK) || access(real_path, X_OK))) // check if the file exists and is executable
-			{
-				found = 1;
-			}
-		}
-
-	free(path);
-	return found;
-}
-*/
-
-/*int log(char **arg)
-{
-	dup2(sil,fileno(stdin));
-}*/
-
+/*//replaced by function with piping support
 int external(char **arg)
 {
 	pid_t pid;
@@ -78,22 +49,20 @@ int external(char **arg)
 	{
 		if(execvp(arg[0],arg)==-1)//execvp searches PATH for arg[0]
 		{
-		//printf("no exec");
 		strcat(temp,arg[0]);
-		printf("temp : %s\targ0 : %s\n",temp,arg[0]);
 		if(execvp(temp,arg)==-1)
-			perror("external exec error");
+			perror("traSH error ");
 	}
 		exit(-1);
 	}
 	else if (pid<0)
-		perror("forking error");
+		perror("trash ");
 	else do
 	{
 		waitpid(pid, &status, 0);
 	} while(!WIFSIGNALED(status)&&!WCOREDUMP(status)&&!WIFEXITED(status));
 	return 1;
-}
+}*/
 
 int ext(char **arg)
 {
@@ -197,7 +166,113 @@ int run(char **arg)
 	else if(strcmp(arg[0],"log")==0)
 		return log(arg);
 	else
-		return external(arg);
+		return do_command(arg, 1);
+}
+
+int do_command(char **args, int pipes) 
+{
+    // The number of commands to run
+    const int commands = pipes + 1;
+    int i = 0;
+
+    int pipefds[2*pipes];
+
+    for(i = 0; i < pipes; i++)
+	{
+        if(pipe(pipefds + i*2) < 0) 
+		{
+            perror("Couldn't Pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    int pid;
+    int status;
+
+    int j = 0;
+    int k = 0;
+    int s = 1;
+    int place;
+    int commandStarts[10];
+    commandStarts[0] = 0;
+
+    // This loop sets all of the pipes to NULL
+    // And creates an array of where the next
+    // Command starts
+
+    while (args[k] != NULL)
+	{
+        if(!strcmp(args[k], "|"))
+		{
+            args[k] = NULL;
+            // printf("args[%d] is now NULL", k);
+            commandStarts[s] = k+1;
+            s++;
+        }
+        k++;
+    }
+
+
+
+    for (i = 0; i < commands; ++i) 
+	{
+        // place is where in args the program should
+        // start running when it gets to the execution
+        // command
+        place = commandStarts[i];
+
+        pid = fork();
+        if(pid == 0) 
+		{
+            //if not last command
+            if(i < pipes)
+			{
+                if(dup2(pipefds[j + 1], 1) < 0)
+				{
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            //if not first command&& j!= 2*pipes
+            if(j != 0 )
+			{
+                if(dup2(pipefds[j-2], 0) < 0)
+				{
+                    perror("dup2");
+                    exit(EXIT_FAILURE);
+                }
+            }
+
+            int q;
+            for(q = 0; q < 2*pipes; q++)
+			{
+                    close(pipefds[q]);
+            }       
+            if( execvp(args[place], args+place) < 0 )
+			{
+                    perror(*args);
+                    exit(EXIT_FAILURE);
+            }
+        }
+        else if(pid < 0)
+		{
+            perror("error");
+            exit(EXIT_FAILURE);
+        }
+
+        j+=2;
+    }
+
+    for(i = 0; i < 2 * pipes; i++)
+	{
+        close(pipefds[i]);
+    }
+
+    for(i = 0; i < pipes + 1; i++)
+	{
+        wait(&status);
+    }
 }
 
 int main(int argc, char **argv)
@@ -205,11 +280,6 @@ int main(int argc, char **argv)
 	char *input;
 	char **arg;
 	int ret;
-	if(sil < 0)
-	{
-		fprintf(stderr,"\nsil file open ret -ve\n");
-		return 1;
-	}
 	while(1)
 	{
 		printf("traSH $ ");
